136f028778575c6912dccac2b2ab2c9c6f85763f
Svavar Berg <svavar15@ru.is>
2017-10-09 13:22:59 +0000

    First commit

---
+++ src/test/java/is/ru/stringcalulator/stringcalculatortest.java
@@ -0,0 +1,13 @@
+package is.ru.stringcalulator
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testEmptyString(){
+		assertEquals(0, Calculator.add(""));
+	}
+
+}
