60b4572ef6cc3083db60dfa2bf634bef2c6bf7d0
Svavar15 <svavar15@ru.is>
2017-10-15 19:53:15 +0000

    Breytt NL fall

--- src/main/java/is/ru/stringcalulator/stringcalculator.java
+++ src/main/java/is/ru/stringcalulator/stringcalculator.java
@@ -6,19 +6,17 @@ public class Calculator {
 		if (text.equals("")){
 			return 0;
 		}
-		else if text.contains(","){
+		if (text.contains("\n")){
+			string numArr[] = text.split("\n");
+			return sum(numArr);
+		}
+		if (text.contains(",")){
 				string numArr[] = text.split(",");
 				return sum(numArr);
 			}
 		}
-		else if text.contains(""){
 
-		}
+	}
 
-		return 1;
-	}
-	private static String[] splitNewLine(string text){
-		return text.split("\n");
-	}
 }
 
